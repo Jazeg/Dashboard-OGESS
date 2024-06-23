17d69125c24177732faa0111ad790c5498b586ac
Jazeg <wilwaeb@hotmail.com>
2024-06-23 02:35:32 -0500

    Hber que sale gaaaa (side-nav)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { GeneralComponent } from './components/general/general.component';
+import { IndicadoresComponent } from './components/indicadores/indicadores.component';
+import { TableroMandoComponent } from './components/tablero-mando/tablero-mando.component';
+import { PerformansComponent } from './components/performans/performans.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    GeneralComponent,
+    IndicadoresComponent,
+    TableroMandoComponent,
+    PerformansComponent
   ],
   imports: [
     BrowserModule,
